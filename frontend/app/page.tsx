e92882bdb7275301e18b3354763fd06fb6de3364
Facu Ramirez <ramirezfacundoo01@gmail.com>
2025-11-18 20:58:28 -0300

    Add frontend and backend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,23 @@
+import  HeroSection from "@/components/ui/hero-section";
+import { getHomePage } from "@/lib/strapi";
+
+export async function generateMetadata() {
+  const strapiData = await getHomePage()
+  return {
+    title: strapiData.title,
+    description: strapiData.description,
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getHomePage();
+
+  const { title, description } = strapiData
+  const [heroSection] = strapiData?.blocks || []
+
+  return (
+    <main className="container mx-auto py-6">
+      <HeroSection data={heroSection}/>
+    </main>
+  );
+}
